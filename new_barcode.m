38a6c2366ae5869a37164e01bfcceed9b63b6369
Darklight-03 <bdc3142@gmail.com>
2017-04-13 13:44:54 -0500

    fixed strcat problem

--- new_barcode.m
+++ new_barcode.m
@@ -1,6 +1,10 @@
 clear all;
 barcodeMotorPort = 'A';
 barcodeSensorPort = 1;
+marbleSensorPort = 2;
+marbleLauncherPort = 'B';
+cupSpinnerPort='C';
+dumperPort='D';
 rotationDistance = 360;
 rotationSpeed = -42;
 interval = 30;
@@ -10,6 +14,12 @@ numBarcodes=3;
 brick = legoev3('usb');
 barcodeMotor = motor(brick, barcodeMotorPort);
 barcodeSensor= colorSensor(brick, barcodeSensorPort);
+%marbleSensor = colorSensor(brick, marbleSensorPort);
+%marbleLauncher = motor(brick,marbleLauncherPort);
+%cupSpinner = motor(brick,cupSpinnerPort);
+%dumper = motor(brick,dumperPort);
+
+
 map = containers.Map;
 
 for i = 1:numBarcodes
@@ -17,3 +27,4 @@ for i = 1:numBarcodes
 	tmap = createList(binary);
 	map = [map ; tmap];
 end
+
